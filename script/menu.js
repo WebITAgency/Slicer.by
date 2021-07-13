81214b0a1fa68fbcc6aff08519e3c3b0aa6ba4fe
Kirill Maximchik <78115664+Flut9@users.noreply.github.com>
2021-07-13 16:18:22 +0300

    0.0.12 beautifyed
    Co-Authored-By: Alexey Yatsevich <76121473+MarshallBaby@users.noreply.github.com>

--- script/menu.js
+++ script/menu.js
@@ -8,24 +8,24 @@ console.log($burgerBtn);
 
 $burgerBtn.addEventListener('click', showMenu);
 
-function show($element){
-    $element.style.display = 'flex';
+function show($element) {
+  $element.style.display = 'flex';
 }
 
-function hide($element){
-    $element.style.display = 'none';
+function hide($element) {
+  $element.style.display = 'none';
 }
 
-function showMenu(){
-    if ($menu.style.display === 'flex'){
-        hide($menu);
-        $burgerBtn.classList.add('bi-list');
-        $burgerBtn.classList.remove('bi-x');
-    } else {
-        show($menu);
-        $burgerBtn.classList.add('bi-x');
-        $burgerBtn.classList.remove('bi-list');
-    }
+function showMenu() {
+  if ($menu.style.display === 'flex') {
+    hide($menu);
+    $burgerBtn.classList.add('bi-list');
+    $burgerBtn.classList.remove('bi-x');
+  } else {
+    show($menu);
+    $burgerBtn.classList.add('bi-x');
+    $burgerBtn.classList.remove('bi-list');
+  }
 }
 
 
@@ -36,17 +36,17 @@ let $arrow = document.querySelector('.catalog__btn i');
 
 $categoriesBtn[1].addEventListener('click', showCategories);
 
-function showCategories(event){
-    event.preventDefault();
-    if ($categories.style.display === 'flex'){
-        hide($categories);
-        $categoriesBtn[1].querySelector('span').textContent = 'Все категории';
-        $arrow.classList.remove('bi-arrow-up');
-        $arrow.classList.add('bi-arrow-down');
-    } else {
-        show($categories);
-        $categoriesBtn[1].querySelector('span').textContent = 'Свернуть';
-        $arrow.classList.add('bi-arrow-up');
-        $arrow.classList.remove('bi-arrow-down');
-    }
+function showCategories(event) {
+  event.preventDefault();
+  if ($categories.style.display === 'flex') {
+    hide($categories);
+    $categoriesBtn[1].querySelector('span').textContent = 'Все категории';
+    $arrow.classList.remove('bi-arrow-up');
+    $arrow.classList.add('bi-arrow-down');
+  } else {
+    show($categories);
+    $categoriesBtn[1].querySelector('span').textContent = 'Свернуть';
+    $arrow.classList.add('bi-arrow-up');
+    $arrow.classList.remove('bi-arrow-down');
+  }
 }
