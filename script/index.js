86284dc4f4a1017d48e688cbf48a8d0934f7b06f
Alexey Yatsevich <76121473+MarshallBaby@users.noreply.github.com>
2021-07-14 13:33:09 +0300

    0.0.14 slider

--- script/index.js
+++ script/index.js
@@ -1,6 +1,14 @@
-document.addEventListener("DOMContentLoaded", ready);
+glide = new Glide('.glide', {
+  type: 'carousel',
+  startAt: 0,
+  autoplay: 7000,
+  hoverpause: true,
+  keyboard: true,
+})
+.mount();
 
-function ready() {
-  let sliderTitle = document.getElementsByClassName("slider__title")[0];
-  sliderTitle.style.left = "calc(50% - " + Math.round(sliderTitle.offsetWidth / 2) + "px)";
-}
+ document.querySelector(".slider__topic-btn").addEventListener('click', () => {
+   document.location.href =
+     document.getElementsByClassName("slider__item-content")[glide.index]
+     .getAttribute("data-href");
+ });
