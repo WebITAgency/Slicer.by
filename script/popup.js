f459bd7e0df4570971e663a4d76dfbfd8ee10fd7
Kirill Maximchik <78115664+Flut9@users.noreply.github.com>
2021-07-19 13:55:04 +0300

    Pupup fixed
    Co-Authored-By: Alexey Yatsevich <76121473+MarshallBaby@users.noreply.github.com>

--- script/popup.js
+++ script/popup.js
@@ -1,10 +1,10 @@
-let $popupWindow = document.querySelector('.popup');
-let $closeBtn = document.querySelector('.card__close');
+let popupWindow = document.querySelector('.popup');
+let closeBtn = document.querySelector('.card__close');
 
-$closeBtn.addEventListener('click', closePopup);
+closeBtn.addEventListener('click', closePopup);
 
 function closePopup(){
-    $popupWindow.style.display = 'none';
+    popupWindow.style.display = 'none';
     document.body.style.overflowY = 'scroll';
 }
 
@@ -13,11 +13,11 @@ document.addEventListener('click', showPopup);
 function showPopup(event){
     if (event.target.classList.contains('product-item__btn')){
         event.preventDefault();
-        $popupWindow.style.display = 'flex';
+        popupWindow.style.display = 'flex';
         document.body.style.overflowY = 'hidden';
-        $popupWindow.querySelector('h2').textContent = event.target.parentElement.querySelector('h2').textContent;
-        $popupWindow.querySelector('span').textContent = event.target.parentElement.querySelector('span').textContent;
-        $popupWindow.querySelector('img').setAttribute('src', event.target.parentElement.querySelector('img').getAttribute('src'));
+        popupWindow.querySelector('h2').textContent = event.target.parentElement.querySelector('h2').textContent;
+        popupWindow.querySelector('span').textContent = event.target.parentElement.querySelector('span').textContent;
+        popupWindow.querySelector('img').setAttribute('src', event.target.parentElement.querySelector('img').getAttribute('src'));
         console.log(event.target.parentElement.querySelector('img'));
     }
 }
