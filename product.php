56168bdd78200dc9c93d76919919521d30a085b3
Alexey Yatsevich <76121473+MarshallBaby@users.noreply.github.com>
2021-07-27 12:45:25 +0300

    wp 0.0.5 product.php

--- product.php
+++ product.php
@@ -1,157 +1,82 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package slicer
+ * Template name: Product Page
+ * Template Post Type: post, page, product
+ */
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+get_header();
+?>
 
-  <link rel="stylesheet" href="css/style.css" />
+<?php 
+  while(have_posts()){
+    the_post();
 
-  <!-- BOOTSTRAP LINK -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-
-  <!-- Required Core Stylesheet -->
-  <link rel="stylesheet" href="node_modules/@glidejs/glide/dist/css/glide.core.min.css" />
-
-  <!-- Optional Theme Stylesheet -->
-  <link rel="stylesheet" href="node_modules/@glidejs/glide/dist/css/glide.theme.min.css" />
-
-  <title>Document</title>
-</head>
-
-<body>
-  <header class="container-fluid d-flex justify-content-center align-items-center header">
-    <div class="row w-100 d-flex justify-content-between align-items-center">
-      <div class="col p-0 header__phonenumber d-lg-none">
-        <i class="bi bi-telephone"></i>
-        <a href="#"> +375 (162) 35 64 45 </a>
-      </div>
-      <div class="p-0 header__desktop-menu align-items-center col d-none d-lg-flex">
-
-        <ul class="header__desktop-links list-unstyled d-flex align-items-center p-0 m-0">
-          <li><a href="#"> <i class="bi bi-house"></i> </a></li>
-          <li><a href="#"> Контакты </a></li>
-          <li><a href="#"> Каталог </a></li>
-          <li><a href="#"> ВАЛВИТ </a></li>
-        </ul>
-      </div>
-      <div class="col d-flex justify-content-end align-items-center p-0">
-        <form class="d-none d-lg-flex header__search-form">
-          <input class="header__search-field" placeholder="Поиск" type="search" name="" id="">
-          <button class="header__search-submit" type="submit"> <i class="bi bi-search"></i> </button>
-        </form>
-        <!-- <div class="header__search d-lg-none">
-              <i class="bi bi-search"></i>
-            </div> -->
-        <div class="header__menu d-lg-none">
-          <i class="header__menu_icon bi bi-list"></i>
-          <div class="header__menu-mobile header__menu-mobile--inactive container-fluid">
-            <ul class="d-flex w-100 flex-column list-unstyled align-items-center justify-content-center container">
-              <li><a href="#"> Контакты </a></li>
-              <li><a href="#"> Каталог </a></li>
-              <li><a href="#"> ВАЛВИТ </a></li>
-              <form class="mt-4 header__search-form">
-                <input class="header__search-field" placeholder="Поиск" type="search" name="" id="">
-                <button class="header__search-submit" type="submit"> <i class="bi bi-search"></i> </button>
-              </form>
-            </ul>
-
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </header>
-
-  <section class="product container">
+    ?>
+      <section class="product container">
     <div class="row mt-5 d-flex justify-content-center justify-content-md-start">
       <div class="col-10 col-md-4 d-flex justify-content-center justify-content-md-start">
-        <img class="product__thumb" src="img/A812T 1.png" alt="">
+        <img class="product__thumb" src="<?php the_post_thumbnail_url() ?>" alt="">
       </div>
       <div class="col-11 mt-3 mt-md-0 col-md-7 d-flex flex-column">
-        <h1 class="product__title"> Слайсер SL-228 EV-5 </h1>
-        <h2 class="product__price"> Цена договорная </h2>
+        <h1 class="product__title"> <?php the_title(); ?> </h1>
+        <h2 class="product__price"> <?php the_excerpt() ?> </h2>
         <a href="#" class="product__button product-item__btn">Заказать</a>
       </div>
     </div>
     <div class="row mt-2 mt-md-5 d-flex justify-content-center justify-content-md-start">
-      <div class="col-11 col-md-7">
+      <div class="col-11 col-md-7 product__description">
         <p class="product__description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna
-          aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-          aute irure dolor in reprehenderit in voluptate velit esse cillum
-          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-          non proident, sunt in culpa qui officia deserunt mollit anim id est
-          laborum.
+          <?php the_content(); ?>
         </p>
-        <ul class="card__description">
+        <!-- <ul class="card__description">
           <li class="description__item"><i class="bi bi-dot"></i>Lorem ipsum dolor</li>
           <li class="description__item"><i class="bi bi-dot"></i>Lorem ipsum dolor</li>
           <li class="description__item"><i class="bi bi-dot"></i>Lorem ipsum dolor</li>
           <li class="description__item"><i class="bi bi-dot"></i>Lorem ipsum dolor</li>
-        </ul>
+        </ul> -->
       </div>
     </div>
   </section>
+    <?php
+  }
+?>
 
-  <section class="container">
-    <h2 class="text-center col-12 section-title">
-      Похожие товары
-    </h2>
-    <div class=" products__container
-     row
-      d-flex
-       justify-content-center
-        justify-content-md-around
-        ">
-      <div class="product-item col-10 col-md-5 col-xxl-3">
-        <img src="img/A812T 1.png" alt="">
-        <h2>Слайсер SL-228 EV-5</h2>
-        <div class="line"></div>
-        <span class="product-item__price">Цена договорная</span>
-        <a href="#" class="product-item__btn">Ознакомиться</a>
-      </div>
+<div class="recommendations">
 
-      <div class="product-item col-10 col-md-5 col-xxl-3">
-        <img src="img/A812T 1.png" alt="">
-        <h2>Слайсер SL-228 EV-5</h2>
-        <div class="line"></div>
-        <span class="product-item__price">Цена договорная</span>
-        <a href="#" class="product-item__btn">Ознакомиться</a>
-      </div>
+<h2>Мы предлагаем</h2>
 
+<div class="products">
+  <div class="products__container container d-flex justify-content-center">
+	<div class="row d-flex justify-content-around w-100">
+    <?php 
+      $posts = get_posts(array(
+        'category_name' => 'product-index',
+        'numberposts' => 3,
+      ));
+      foreach ($posts as $post) {
+        ?>
+        <div class="product-item col-10 col-xxl-3">
+		<img src="<?php the_post_thumbnail_url() ?>" alt="">
+		<h2><?php the_title(); ?></h2>
+		<div class="line"></div>
+		<span class="product-item__price"><?php the_excerpt(); ?></span>
+		<a href="<?php the_permalink() ?>" class="product-item__btn">Ознакомиться</a>
+	  </div>
+        <?php 
+      }
+    ?>
+	</div>
+  </div>
+</div>
 
-      <div class="product-item col-10 col-md-5 col-xxl-3">
-        <img src="img/A812T 1.png" alt="">
-        <h2>Слайсер SL-228 EV-5</h2>
-        <div class="line"></div>
-        <span class="product-item__price">Цена договорная</span>
-        <a href="#" class="product-item__btn">Ознакомиться</a>
-      </div>
+<a href="#" class="recommendation__link">наверх</a>
 
-    </div>
-  </section>
+</div>
 
-  <footer class="footer">
-    <div class="partners">
-      <img src="img/dick-white 1.png" alt="" class="partners__item">
-      <img src="img/Giesser_Messer_magazin_posudy_posuda_dlya_restoranov_professionalnaya.png" alt="" class="partners__item">
-      <img src="img/mainca 1.png" alt="" class="partners__item">
-      <img src="img/roser-group.png" alt="" class="partners__item">
-      <img src="img/eicker 1.png" alt="" class="partners__item">
-    </div>
-  </footer>
-
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-  <script src="node_modules/@glidejs/glide/dist/glide.min.js"></script>
-  <script src="script/glideJsMount.js"> </script>
-  <script src="script/menu.js"></script>
-  <script src="script/index.js"></script>
-</body>
-
-</html>
+<?php
+get_footer();
